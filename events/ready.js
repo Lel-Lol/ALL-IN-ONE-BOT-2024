dcd00acbb150e70587c703b337ddc03410589deb
Lel-Lol <77671401+Lel-Lol@users.noreply.github.com>
2024-09-16 00:05:13 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -8,7 +8,7 @@ module.exports = {
         const activities = [
             { name: 'Netflix', type: ActivityType.Watching },
             { name: 'GTA VI', type: ActivityType.Playing },
-            { name: 'on YouTube', type: ActivityType.Emoting },
+            { name: 'on YouTube', type: ActivityType.Streaming },
             { name: 'Spotify', type: ActivityType.Listening },
         ];
 
